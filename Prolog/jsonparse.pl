aa9c31b657450857df9eb710ce1c17cd4d4fe194
Daniele <77250486+DanieleDituri@users.noreply.github.com>
2023-01-07 18:53:12 +0100

    Added jsondump(JSON, FileName)

--- Prolog/jsonparse.pl
+++ Prolog/jsonparse.pl
@@ -12,13 +12,19 @@
 % cio che dobbiamo gestire: 
 % \n = 10,
 % spazio bianco = 32.    // alcuni
-% " = 34,
+% '"' = 34,
 % , = 44,
 % : = 58,   
 % [ = 91, 
 % ] = 93,
 % { = 123,    
 % } = 125. 
+% simboli utili per i numeri sono
+% + = 43,
+% - = 45,
+% . = 46,
+% E = 69,
+% e = 101.
 
 %% jsonparse
 % Caso base principale:
@@ -84,59 +90,81 @@ parsestr([34 | Xs], [], Xs).
 parsestr([X | Xs], [X | Zs], Ys) :-
     parsestr(Xs, Zs, Ys).
 
+
 % gestistione di tutti i tipi di value, quindi tutto ciò che c'è dopo i :
 parseval([58 | Xs], Value, MoreMembers) :-
     delspace(Xs, X),
     parsevalue(X, Value, MoreMembers).
 parsevalue([58 | _], _, _) :- !, fail.
+
 % stringa
 parsevalue([34 | Xs], Value, MoreMembers) :-
     parsestring([34 | Xs], Value, MoreMembers).
-% number
+
+% number semblice o con un segno d'avanti
+% number con secgno +
 parsevalue([43 | Xs], Value, MoreMembers) :-
     parsenumber(Xs, ValueCodes, MoreMembers),
     atom_string([43 | ValueCodes], ValueString),
-    number_string(Value, ValueString).
+    number_string(Value, ValueString).    
+% number con segno -
 parsevalue([45 | Xs], Value, MoreMembers) :-
     parsenumber(Xs, ValueCodes, MoreMembers),
     atom_string([45 | ValueCodes], ValueString),
     number_string(Value, ValueString).
+% number semplice
 parsevalue([X | Xs], Value, MoreMembers) :-
     between(48, 57, X),
     parsenumber([X | Xs], ValueCodes, MoreMembers),
     atom_string(ValueCodes, ValueString),
     number_string(Value, ValueString).
+
 % object
 parsevalue([123 | Xs], Members, MoreMembers) :-
     parsing([123 | Xs], Members),
     delparg(Xs, MoreMembers).
+
 % array 
-% cancella la parentesi aperta [, salva gli elementi e cancella la parentesi chiusa ]
+% trova e cancella la parentesi aperta [, salva gli elementi 
+% cancella la parentesi chiusa ]
 parsevalue([91 | Xs], jsonarray(Elements), MoreMembers) :-
     parseelements(Xs, Elements),
     delparq(Xs, MoreMembers).
 
-% se è un numero va vanti e infine salvera tutto
+% un numero può essere scritto così:     +1.3e-2   e   -1.5E3 
 parsenumber([X | Xs], [X | Ys], Other) :-
     between(48, 57, X),
     parsenumber(Xs, Ys, Other).
+
+% numero con la virgola
 parsenumber([46 | Xs], [46 | Ys], Other) :-
     parsenumbervirg(Xs, Ys, Other).
+% numero con la E
 parsenumber([69 | Xs], [69 | Ys], Other) :-
     parsenumberel(Xs, Ys, Other).
+% numero con la e
 parsenumber([101 | Xs], [101 | Ys], Other) :-
     parsenumberel(Xs, Ys, Other).
+% caso base in cui hai letto tutto
 parsenumber(Xs, [], Xs).
 
+%% parsenumbervirg
+% numeri con la virgola 
 parsenumbervirg([X | Xs], [X | Ys], Other) :-
     between(48, 57, X),
     parsenumbervirg(Xs, Ys, Other).
+% caso in cui numero virgola ed E (-1.5E3)
 parsenumbervirg([69 | Xs], [69 | Ys], Other) :-
     parsenumberel(Xs, Ys, Other).
+% caso in cui numero virgola ed e (+1.3e-2) 
 parsenumbervirg([101 | Xs], [101 | Ys], Other) :-
     parsenumberel(Xs, Ys, Other).
+% % caso base in cui hai letto tutto
 parsenumbervirg(Xs, [], Xs).
 
+%% parsenumberel
+% caso in cui numero virgola ed e (+1.3e-2) 
+% dopo può esserci un numero semplice con segno
 parsenumberel([X | Xs], [X | Ys], Other) :-
     between(48, 57, X),
     parsenumberel2(Xs, Ys, Other).
@@ -144,7 +172,6 @@ parsenumberel([43 | Xs], [43 | Ys], Other) :-
     parsenumberel2(Xs, Ys, Other).
 parsenumberel([45 | Xs], [45 | Ys], Other) :-
     parsenumberel2(Xs, Ys, Other).
-
 parsenumberel2([X | Xs], [X | Ys], Other) :-
     between(48, 57, X),
     parsenumberel2(Xs, Ys, Other).
@@ -164,11 +191,19 @@ delspace([32 | Xs], Ys) :-
 delspace(Xs, Xs).
 
 % Cancella le parentesi chiuse }
+delparg([123 | Xs], Ys) :-
+    !,
+    delparg(Xs, Zs),
+    delparg(Zs, Ys).
 delparg([125 | Xs], Xs).
 delparg([_ | Xs], Ys) :-
     delparg(Xs, Ys).
 
 % Cancella le parentesi chiuse ]
+delparq([91 | Xs], Ys) :-
+    !,
+    delparq(Xs, Zs),
+    delparq(Zs, Ys).
 delparq([93 | Xs], Xs).
 delparq([_ | Xs], Ys) :-
     delparq(Xs, Ys).
@@ -183,6 +218,85 @@ jsonread(FileName, JSON) :-
     read_stream_to_codes(In, X),
     close(In),
     jsonparse(X, JSON).
+
+jsondump(JSON, FileName) :-
+    open(FileName, write, Out),
+    writejson(JSON, String),
+    write(Out, String),
+    close(Out).
+
+writejson(jsonobj([]), String) :-
+    String = "{}".
+
+writejson(jsonarray([]), String) :-
+    String = "[]".
+
+writejson(JSON, String) :-
+    jsonobj([X | Xs]) = JSON,
+    !,
+    string_concat("{", "\n", Str1),
+    writemembers([X | Xs], Str2),
+    string_concat(Str1, Str2, Str3),
+    string_concat(Str3, "\n}", String).
+
+writejson(JSON, String) :-
+    jsonarray([X | Xs]) = JSON,
+    !,
+    string_concat("[", "", Str1),
+    writeelements([X | Xs], Str2),
+    string_concat(Str1, Str2, Str3),
+    string_concat(Str3, "]", String).
+
+writemembers([], String) :-
+    String = "".
+writemembers([(Attribute, Value) | Xs], String) :-
+    string_concat('\"', Attribute, Str1),
+    string_concat(Str1, '\"', Str2),
+    string_concat(Str2, " : ", Str3),
+    writevalue(Value, Str4),
+    string_concat(Str3, Str4, Str5),
+    checkwrite(Xs, Str6),
+    string_concat(Str5, Str6, Str7),
+    writemembers(Xs, Str8),
+    string_concat(Str7, Str8, String).
+
+checkwrite([], "").
+checkwrite([_ | _], ",\n").
+
+writeelements([], String) :-
+    String = "".
+writeelements([Value | Xs], String) :-
+    writevalue(Value, Str1),
+    checkarray(Xs, Str2),
+    string_concat(Str1, Str2, Str3),
+    writeelements(Xs, Str4),
+    string_concat(Str3, Str4, String).
+
+checkarray([], "").
+checkarray([_ | _], ", ").
+
+writevalue(jsonobj([]), "{}").
+writevalue(Value, String) :-
+    jsonobj([X | Xs]) = Value,
+    !,
+    writejson(jsonobj([X | Xs]), String).
+
+writevalue(jsonarray([]), "[]").
+writevalue(Value, String) :-
+    jsonarray([X | Xs]) = Value,
+    !,
+    writejson(jsonarray([X | Xs]), String).
+
+writevalue(Value, String) :-
+    string(Value),
+    !,
+    string_concat('\"', Value, Str1),
+    string_concat(Str1, '\"', String).
+
+writevalue(Value, String) :-
+    number(Value),
+    !,
+    number_string(Value, String).
 
 % controllo delle parentisi {[]} 
 % conta numero parentisi aperte { e conta le chiuse }
@@ -234,32 +348,37 @@ chiuseq([X | Xs], C) :-
     chiuseq(Xs, C).
 
 
-% jsonaccess(Jsonobj, Fields, Result)
+%% jsonaccess(Jsonobj, Fields, Result)
+% i diversi casi base in cui deve fallire
 jsonaccess(jsonobj(Members), [], jsonobj(Members)) :- !.
 jsonaccess(jsonarray(), [], _) :- !, fail.
 jsonaccess(jsonarray([]), [], _) :- !, fail.
 
+% scritto correttamente di tutti i 3 possibili modi in cui può essere scritto
 jsonaccess(Jsonobj, X, Result) :-
-    searchelements(Jsonobj, X, Result).
-
+    searchattributes(Jsonobj, X, Result).
 jsonaccess(Jsonobj, [X], Result) :-
-    searchelements(Jsonobj, X, Result).
-
+    searchattributes(Jsonobj, X, Result).
 jsonaccess(Jsonobj, [X | Xs], Result) :-
-    searchelements(Jsonobj, X, TMP),
+    searchattributes(Jsonobj, X, TMP),
     !,
     jsonaccess(TMP, Xs, Result).
 
-searchelements(Obj, X, Result) :-
+%% searchattributes(Object, Fields, Result)
+% ricerca tra gli attribute quello uguale a quello richiesto (X)
+% cerca il suo value 
+% caso di un oggetto
+searchattributes(Obj, X, Result) :-
     jsonobj([Y | Ys]) = Obj,
     !,
     searchvalue([Y | Ys], X, Result).
-
-searchelements(Array, X, TMP) :-
+% caso di un array
+searchattributes(Array, X, TMP) :-
     jsonarray([Y | Ys]) = Array,
     !,
     searchkeyvalue([Y | Ys], X, TMP).
 
+% salva il value nel Result se gli attribute sono uguali
 searchvalue([], _, _) :- !, fail.
 searchvalue([(Attribute, Value) | _], Attribute, Result) :-
     !,
@@ -267,10 +386,12 @@ searchvalue([(Attribute, Value) | _], Attribute, Result) :-
 searchvalue([_ | Xs], X, Result) :-
     searchvalue(Xs, X, Result).
 
+% cerca nell'array il value interessato in base al il numero inserito nel comando (key)
 searchkeyvalue([], [_], _) :- !, fail.
 searchkeyvalue([Y | _], 0, Result) :-
     !,
     Result = Y.
+% la ricerca la fa con un contatore che si decrementa in base alla key
 searchkeyvalue([_ | Ys], X, Result) :-
     Z is X - 1,
     searchkeyvalue(Ys, Z, Result).
